460256b9f73818319038e565e8bc1e9eaa241c9c
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-02-21 18:34:54 +0300

    '.data' has data for every page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,20 @@
 <script>
   import { navigating, page, updated } from '$app/stores';
-
   const colors = ['ff3e00', '676779', '42b4ff', 'invalid'];
 </script>
+
+<!-- 
+  As this is the top-level layout, we always have access to the current page data through the $page store.
+  The same data is also available in the "$page" store in the current page component.
+  While setting the export "let data" in the route +page.svelte files is not required, 
+  it does enhance code completion and type checking.
+
+  For example, to render the component returned from the load function of the "component" route, we can use:
+
+  {#if $page.data.component}
+    <svelte:component this={$page.data.component} />
+  {/if}
+ -->
 
 <nav>
   <a href="/" aria-current={$page.url.pathname === '/'}>home</a>
@@ -10,6 +22,7 @@
   <a href="/about" aria-current={$page.url.pathname === '/about'}>about</a>
   <a href="/account" aria-current={$page.url.pathname === '/account'}>account</a>
   <a href="/app" aria-current={$page.url.pathname === '/app'}>app</a>
+  <a href="/component" aria-current={$page.url.pathname === '/component'}>component</a>
 </nav>
 
 <h1>Color URLs</h1>
