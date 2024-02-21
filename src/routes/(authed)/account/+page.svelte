2bcc89b688452fa9f1d91d90ea0075a75d8567f3
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-02-21 14:33:41 +0300

    add authed pages - account and app

---
+++ src/routes/(authed)/account/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let data;
+</script>
+
+<h1>account</h1>
+
+<p>this is the account page</p>
+
+<p>Logged in as: {data.user.name}</p>
---
+++ src/routes/(authed)/app/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let data;
+</script>
+
+<h1>app</h1>
+
+<p>this is the app</p>
+
+<p>Logged in as: {data.user.name}</p>
