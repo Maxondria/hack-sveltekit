542a27dcaff2cf6c885a55f33a8a92c35ee539ee
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-02-21 14:34:05 +0300

    update the nav layout for all existing routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,23 @@
 <script>
   import { navigating, page, updated } from '$app/stores';
+
+  const colors = ['ff3e00', '676779', '42b4ff', 'invalid'];
 </script>
 
 <nav>
   <a href="/" aria-current={$page.url.pathname === '/'}>home</a>
   <a href="/blog" aria-current={$page.url.pathname === '/blog'}>blog</a>
+  <a href="/about" aria-current={$page.url.pathname === '/about'}>about</a>
+  <a href="/account" aria-current={$page.url.pathname === '/account'}>account</a>
+  <a href="/app" aria-current={$page.url.pathname === '/app'}>app</a>
+</nav>
+
+<h1>Color URLs</h1>
+
+<nav class="color">
+  {#each colors as color}
+    <a href="/colors/{color}" style="--color: #{color}">#{color}</a>
+  {/each}
 </nav>
 
 {#if $navigating}
@@ -20,3 +33,18 @@
 {/if}
 
 <slot />
+
+<style>
+  .color a {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  .color a::before {
+    content: '';
+    width: 2em;
+    height: 2em;
+    background: var(--color);
+  }
+</style>
